b708c2967fd186af1aa4c9e5327381b9b5a6895c
Paulo Henrique Calletti <73306576+PauloCalletti@users.noreply.github.com>
2021-02-09 18:12:39 -0300

    Atualizando nfse segundo recomendações.

--- src/nfse.js
+++ src/nfse.js
@@ -1,30 +1,27 @@
- const data = require("./arquivo/data_nfse.js")
-const axios = require("axios");
-
-//TODO: Externalizar o config
-//TODO: Colocar o conteudo em uma função
-//TODO: Simplificar o data: data
-//TODO: Usar async/await ao invés de promises
-//TODO: Remover pdf e xml do repo
+const nfse = require("./arquivo/data_nfse.js")
+const Axios = require("axios");
+const env = require("../config.js")
 
 require("dotenv/config.js")
 
-const config = {
-  method: "POST",
-  url: (process.env.NFSE_URL),
-  headers: { 
-    "x-api-key": (process.env.X_API_KEY), 
-    "Content-Type": "application/json"
-  },
-  data : data
-};
+async function postNfse() {
+  try {
+    const response = await Axios ({
+    method: "POST",
+    url: env.NFSE_URL,
+    headers: { 
+      "x-api-key": env.X_API_KEY, 
+      "Content-Type": "application/json"
+    },
+    data : nfse,
+    })
+  
+    console.log(JSON.stringify(response.data))
+    return response.data
+  }
+  catch(error){
+    console.error(error)
+  }
+}
 
-axios(config)
-.then(function (response) {
-  console.log(JSON.stringify(response.data));
-})
-
-.catch(
-  console.error()
-);
-
+postNfse();
