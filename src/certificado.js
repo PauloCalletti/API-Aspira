4ddb3dacc3d722b962996198ad393ab67874823b
Paulo Henrique Calletti <73306576+PauloCalletti@users.noreply.github.com>
2021-02-09 18:10:42 -0300

    Atualizando o certificado segundo recomendações.

--- src/certificado.js
+++ src/certificado.js
@@ -2,15 +2,13 @@ const Axios = require("axios");
 const FormData = require("form-data");
 const Fs = require("fs");
 const Path = require("path")
-
-//TODO: Externalizar o config
-//TODO: Colocar senha do certificado no config
+const env = require("../config.js")
 
 require("dotenv/config.js")
 
   async function returnCertificate() {
     try {
-      const url = (process.env.CERTIFY_URL)
+      const url = env.CERTIFY_URL
       const data = new FormData();
       const path = Path.resolve(__dirname, "arquivo", "cert_teste.pfx")
 
@@ -22,7 +20,7 @@ require("dotenv/config.js")
         method: "POST",
         data, 
         headers: {
-          "x-api-key": (process.env.X_API_KEY),
+          "x-api-key": env.X_API_KEY,
           ...data.getHeaders()
         },
       })
