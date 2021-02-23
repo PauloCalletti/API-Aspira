852ad4ca687a89fe268d5675f7c0fc50c0d03884
Paulo Henrique Calletti <73306576+PauloCalletti@users.noreply.github.com>
2021-02-23 13:29:32 -0300

    Atualizado config

--- config.js
+++ config.js
@@ -2,37 +2,10 @@ require('dotenv').config
 
 const env = process.env
 
-post:{
-   url:env.POST_URL
-   headers: {
-      x_api_key: env.X_API_KEY
-   }
-}
-certificado:{
-   url:env.CERTIFICADO_URL
-   headers: {
-      x_api_key: env.X_API_KEY
-   }
-}
-nfse:{
-   url:env.NFSE_URL
-   headers: {
-      x_api_key: env.X_API_KEY
-   }
-}
-pdf:{
-   url:env.PDF_URL
-   headers: {
-      x_api_key: env.X_API_KEY
-   }
-}
-xml:{
-   url:env.XML_URL
-   headers: {
-      x_api_key: env.X_API_KEY
-   }
-}
+url: env.URL_PLUG;
+xkey: env.X_API_KEY; 
 password: {
    senha: env.CERT_PASSWORD
 }
+
 module.exports = env;
