4a846ba1d3009e3f70a77e2576402736592fc706
Paulo Henrique Calletti <73306576+PauloCalletti@users.noreply.github.com>
2021-02-23 13:23:02 -0300

    Ultima atualização NFSE.

--- src/nfse.js
+++ src/nfse.js
@@ -1,27 +1,27 @@
-const nfse = require("./arquivo/data_nfse.js")
 const Axios = require("axios");
 const env = require("../config.js")
 
 require("dotenv/config.js")
 
-async function postNfse() {
-  try {
-    const response = await Axios ({
-    method: "POST",
-    url: env.NFSE_URL,
-    headers: { 
-      "x-api-key": env.X_API_KEY, 
-      "Content-Type": "application/json"
-    },
-    data : nfse,
-    })
+async function postNfse(data_nfse) {
+   try {
+      const URL = env.URL_PLUG
+      const KEY = env.X_API_KEY
+      const response = await Axios ({
+      method: "POST",
+      url: `${URL}/nfse`,
+      headers: { 
+         "x-api-key": `${KEY}`, 
+         "Content-Type": "application/json"
+      },
+      data : `${data_nfse}`,
+      })
   
-    console.log(JSON.stringify(response.data))
-    return response.data
-  }
-  catch(error){
-    console.error(error)
-  }
+   return response.data
+   }
+   catch(error){
+      throw new Error(error)
+   }
 }
 
-postNfse();
+module.exports = postNfse;
